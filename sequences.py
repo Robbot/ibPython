1f389f5a56591666d0b57379655de696af29a2d1
Rob_R <roju@robertrojek.pl>
2018-03-23 12:52:06 +0000

    23 of March YL

---
+++ sequences.py
@@ -0,0 +1,9 @@
+def main():
+    print(len("pizza"))
+    print(len([1,2,5,7,9,122,2,3]))
+    print(len(["a","e","i","o","u"]))
+    
+    print(max([1,2,5,7,9,122,2,3]))
+    print(min([1,2,5,7,9,122,2,3]))
+    
+main()
