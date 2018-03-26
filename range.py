a0ed3fd55dabfb143aa5b67fc54b317e3dddc3ef
Rob_R <roju@robertrojek.pl>
2018-03-26 13:50:55 +0100

    range and sorted.py added

---
+++ range.py
@@ -0,0 +1,11 @@
+def main():
+    mynums=list(range(10))
+    print(mynums)
+    
+    mynums=list(range(10,20))
+    print(mynums)
+        
+    mynums=list(range(10,20,2))
+    print(mynums)
+main()
+
---
+++ sorted.py
@@ -0,0 +1,8 @@
+def main():
+    
+    print(sorted([1,4,9,8,5,6]))
+    print(sorted(['g','z','s','w','j','f','b','a']))
+    
+    items=['bread', 'milk','cheese','eggs']
+    print(sorted(items))
+main()
