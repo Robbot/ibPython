9f9af91635cf1a85299b1b08ebe27e0ebf7219af
Rob_R <roju@robertrojek.pl>
2018-03-27 12:25:08 +0100

    Some files from Third repo

---
+++ solo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# New lines are done with \n
+#print("test of creation new line\nthis is new line")
+#print("spam" * 3)
+# spamspamspam
+#
+#print (4*'2') # multiply string two four times
+#'2222'
+#print (4*2) # multiply two by four
+#8
+#print (4*int('2')) # multiply string converted to number four times
+#8
+#>>> '17' * '87'
+#TypeError: can't multiply sequence by non-int of type 'str'
+#
+#>>> 'pythonisfun' * 7.0
+#TypeError: can't multiply sequence by non-int of type 'float'
