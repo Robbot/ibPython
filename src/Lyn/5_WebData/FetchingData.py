5e81595efee250b363fb90efd97acef5b1957631
Verwolff <verwolff1@gmail.com>
2018-04-05 20:03:40 +0100

    the fifth

---
+++ src/Lyn/5_WebData/FetchingData.py
@@ -0,0 +1,16 @@
+'''
+Created on 5 kwi 2018
+
+@author: Roju
+'''
+import urllib.request
+
+def main():
+    
+    webUrl = urllib.request.urlopen("http://www.google.com")
+    print("result code: " + str(webUrl.getcode()))
+    data = webUrl.read()
+    print(data)
+    
+if __name__ == "__main__":
+    main()
